09364b166cfd40647ef82a251cf837dc6b77d625
junichi-kose <junichi-kose@peer-connect.co.jp>
2022-03-09 21:09:06 +0900

    クオーテーションを検索するとCB_VA01エラーになっていた問題を回避

--- src/desktop/queryConditonManager.ts
+++ src/desktop/queryConditonManager.ts
@@ -20,29 +20,29 @@ export default class queryCondition {
       } else if (Array.isArray(selfFieldValue)) {
         if (selfFieldValue.length) {
           if (['FILE'].includes(selfFieldType)) {
-            return `${targetField} ${operator} (${(record[selfField] as KintoneRecordField.File).value.map(({ name }) => `"${name}"`).join(', ')})`;
+            return `${targetField} ${operator} (${(record[selfField] as KintoneRecordField.File).value.map(({ name }) => JSON.stringify(name)).join(', ')})`;
           } else if (['USER_SELECT', 'STATUS_ASSIGNEE', 'ORGANIZATION_SELECT', 'GROUP_SELECT'].includes(selfFieldType)) {
-            return `${targetField} ${operator} (${(record[selfField] as KintoneRecordField.UserSelect).value.map(({ code }) => `"${code}"`).join(', ')})`;
+            return `${targetField} ${operator} (${(record[selfField] as KintoneRecordField.UserSelect).value.map(({ code }) => JSON.stringify(code)).join(', ')})`;
           } else {
-            return `${targetField} ${operator} (${selfFieldValue.map(v => `"${v}"`).join(', ')})`;
+            return `${targetField} ${operator} (${selfFieldValue.map(v => JSON.stringify(v)).join(', ')})`;
           }
         } else {
           return `${targetField} ${operator} ("")`;
         }
       } else {
-        return `${targetField} ${operator} ("${selfFieldValue}")`;
+        return `${targetField} ${operator} (${JSON.stringify(selfFieldValue)})`;
       }
     } else {
       if (['CREATOR', 'MODIFIER'].includes(selfFieldType)) {
-        return `${targetField} ${operator} "${(record[selfField] as KintoneRecordField.Creator).value.code}"`;
+        return `${targetField} ${operator} ${JSON.stringify((record[selfField] as KintoneRecordField.Creator).value.code)}`;
       } else if (['FILE'].includes(selfFieldType)) {
-        return `${targetField} ${operator} "${(record[selfField] as KintoneRecordField.File).value.map(({ name }) => name).join('')}"`;
+        return `${targetField} ${operator} (${(record[selfField] as KintoneRecordField.File).value.map(({ name }) => JSON.stringify(name)).join(', ')})`;
       } else if (['USER_SELECT', 'STATUS_ASSIGNEE', 'ORGANIZATION_SELECT', 'GROUP_SELECT'].includes(selfFieldType)) {
         return `${targetField} ${operator} "${(record[selfField] as KintoneRecordField.UserSelect).value.map(({ code }) => code).join('')}"`;
       } else if (['CHECK_BOX', 'MULTI_SELECT', 'CATEGORY'].includes(selfFieldType)) {
-        return `${targetField} ${operator} "${(record[selfField] as KintoneRecordField.CheckBox).value.join('')}"`;
+        return `${targetField} ${operator} ${JSON.stringify((record[selfField] as KintoneRecordField.CheckBox).value.join(','))}`;
       } else {
-        return `${targetField} ${operator} "${selfFieldValue}"`;
+        return `${targetField} ${operator} ${JSON.stringify(selfFieldValue)}`;
       }
     }
   }
